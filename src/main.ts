ea2e5b3ac517d04265ae50ef3eba801b02cec573
lingfengjgf <lingfengjgf@163.com>
2025-02-02 16:18:07 +0800

    搭建环境、加载汽车模型

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,44 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+import { RGBELoader } from 'three/examples/jsm/Addons.js';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
-createApp(App).mount('#app')
+// 创建渲染器
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio); // 像素比
+
+document.body.appendChild(renderer.domElement);
+
+// 创建场景
+const scene = new THREE.Scene();
+
+// const boxGeo = new THREE.BoxGeometry(1, 1, 1);
+// const boxMat = new THREE.MeshBasicMaterial({color: 0x00ff00});
+// const boxMesh = new THREE.Mesh(boxGeo, boxMat);
+// scene.add(boxMesh);
+
+// 创建相机
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 100);
+camera.position.set(0, 0, 5);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const gltfLoader = new GLTFLoader();
+gltfLoader.load('car.glb', (gltf) => {
+    scene.add(gltf.scene);
+})
+
+const rgbeLoader = new RGBELoader();
+rgbeLoader.load('sky.hdr', skyTexture => {
+    scene.background = skyTexture;
+    scene.environment = skyTexture;
+    skyTexture.mapping = THREE.EquirectangularReflectionMapping; // 全景图
+})
+
+renderer.setAnimationLoop(animationLoop);
+
+function animationLoop() {
+    renderer.render(scene, camera);
+    controls.update();
+}
