12abe2817bb5c0c754c068f990150003b13b9255
lingfengjgf <lingfengjgf@163.com>
2025-02-05 19:24:48 +0800

    实现场景动态反射

--- src/main.ts
+++ src/main.ts
@@ -13,20 +13,42 @@ document.body.appendChild(renderer.domElement);
 // 创建场景
 const scene = new THREE.Scene();
 
-// const boxGeo = new THREE.BoxGeometry(1, 1, 1);
-// const boxMat = new THREE.MeshBasicMaterial({color: 0x00ff00});
-// const boxMesh = new THREE.Mesh(boxGeo, boxMat);
-// scene.add(boxMesh);
+/**  实现动态反射环境 **/ 
+// 1 创建WebGLCubeRenderTarget
+const cubeRendererTarget = new THREE.WebGLCubeRenderTarget(1024);
+// 2 将cubeRendererTarget对象作为参数传递给cubeCamera
+const cubeCamera = new THREE.CubeCamera(0.1, 100, cubeRendererTarget);
+// 3 在帧循环中调用 cubeCamera.update ，每帧更新渲染结果
+// 4 cubeRendererTarget对象的texture环境材质的环境贴图通道：envMap
+let ground: any;
+
+const boxGeo = new THREE.BoxGeometry(1, 1, 1);
+const boxMat = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const boxMesh = new THREE.Mesh(boxGeo, boxMat);
+scene.add(boxMesh);
+boxMesh.position.set(0, 3.2, 0);
 
 // 创建相机
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 100);
-camera.position.set(0, 0, 5);
+camera.position.set(0, 1, 5);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
 const gltfLoader = new GLTFLoader();
 gltfLoader.load('car.glb', (gltf) => {
     scene.add(gltf.scene);
+
+    gltf.scene.traverse((child: any) => {
+        if (child.type == "Mesh") {
+            child.material.envMap = cubeRendererTarget.texture;
+            // child.visible = false;
+        }
+        if (child.name == "ground") {
+            // child.material.side = THREE.DoubleSide;
+            // child.visible = true;
+            ground = child;
+        }
+    })
 })
 
 const rgbeLoader = new RGBELoader();
@@ -39,6 +61,15 @@ rgbeLoader.load('sky.hdr', skyTexture => {
 renderer.setAnimationLoop(animationLoop);
 
 function animationLoop() {
-    renderer.render(scene, camera);
     controls.update();
+    
+    if (ground) {
+        ground.visible = false;
+        cubeCamera.position.copy(camera.position);
+        cubeCamera.position.y = -cubeCamera.position.y;
+        cubeCamera.update(renderer, scene);
+        ground.visible = true;
+    }
+
+    renderer.render(scene, camera);
 }
