8f1abfd5e8c940941d703b3a19168e127dfbbd4e
lingfengjgf <lingfengjgf@163.com>
2025-02-08 21:51:49 +0800

    增加隧道飞行粒子动效

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 import { RGBELoader } from 'three/examples/jsm/Addons.js';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import gsap from 'gsap';
 
 // 创建渲染器
 const renderer = new THREE.WebGLRenderer();
@@ -17,7 +18,7 @@ const scene = new THREE.Scene();
 // 1 创建WebGLCubeRenderTarget
 const cubeRendererTarget = new THREE.WebGLCubeRenderTarget(1024);
 // 2 将cubeRendererTarget对象作为参数传递给cubeCamera
-const cubeCamera = new THREE.CubeCamera(0.1, 100, cubeRendererTarget);
+const cubeCamera = new THREE.CubeCamera(0.1, 500, cubeRendererTarget);
 // 3 在帧循环中调用 cubeCamera.update ，每帧更新渲染结果
 // 4 cubeRendererTarget对象的texture环境材质的环境贴图通道：envMap
 let ground: any;
@@ -29,7 +30,7 @@ let ground: any;
 // boxMesh.position.set(0, 3.2, 0);
 
 // 创建相机
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 500);
 camera.position.set(0, 1, 5);
 
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -47,6 +48,25 @@ gltfLoader.load('car.glb', (gltf) => {
             // child.material.side = THREE.DoubleSide;
             // child.visible = true;
             ground = child;
+        }
+        if (child.name == "flyLight") {
+            // 使隧道远处的飞行粒子清晰
+            child.material.map.anisotropy = 8;
+
+            gsap.to(child.material.map.offset, {
+                x: -1,
+                repeat: -1,
+                ease: 'none',
+                duration: 0.5
+            })
+        }
+        if (child.name == "mainCar") {
+            // 使车身变亮
+            child.traverse((item: any) => {
+                if (item.type == "Mesh") {
+                    item.material.envMapIntensity = 5;
+                }
+            })
         }
     })
 })
